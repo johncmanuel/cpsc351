7a8d09f29488f50d53a97de3be3d7650327c2f26
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-09-06 10:51:44 -0700

    hw2 prog

--- hw/hw2/q6.c
+++ hw/hw2/q6.c
@@ -9,7 +9,6 @@
 int main(int argc, const char* argv[]) {
     int p[2];
 
-    // Create pipe
     if (pipe(p) < 0) {
         printf("pipe error");
         exit(1);
@@ -18,32 +17,32 @@ int main(int argc, const char* argv[]) {
     int rc1 = fork();
 
     if (rc1 < 0) { fprintf(stderr, "Fork 1 failed\n"); exit(1); }
-    // Child 1
+    // child 1
     else if (rc1 == 0) {
         close(p[0]);
         dup2(p[1], STDOUT_FILENO);
         close(p[1]);
-        printf("child 1");
-    }
-    // Parent
-    else {
-        wait(NULL);
+
+        char* args[] = { strdup("grep"), strdup("-o"), strdup("fork"), strdup("q4.c"), NULL };
+        execvp(args[0], args);
     }
 
     int rc2 = fork();
 
     if (rc2 < 0) { fprintf(stderr, "Fork 2 failed\n"); exit(1); }
-    // Child 2
+    // child 2
     else if (rc2 == 0) {
         close(p[1]);
         dup2(p[0], STDIN_FILENO);
-        close(p[1]);
-        printf("child 2");
+        close(p[0]);
+
+        char* args[] = { strdup("wc"), strdup("-l"), NULL };
+        execvp(args[0], args);
     }
-    // Parent
-    else {
-        wait(NULL);
-    }
+
+    // close pipe
+    close(p[0]);
+    close(p[1]);
 
     return 0;
 }
