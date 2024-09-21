8eda68f031683d316c814af4a0159ec82af1df64
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-09-20 17:44:35 -0700

    more assignments woo

---
+++ projects/shell/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void parse(char *line, char **argv) {
+  while (*line != '\0') {
+    while (*line == ' ' || *line == '\t' || *line == '\n')
+      *line++ = '\0';
+    *argv++ = line;
+    while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n')
+      line++;
+  }
+  *argv = '\0';
+}
+
+void execute(char **argv) {
+  pid_t pid;
+  int status;
+  if ((pid = fork()) < 0) {
+    printf("Forking child process failed\n");
+    exit(1);
+  } else if (pid == 0) {
+    if (execvp(*argv, argv) < 0) {
+      printf("Execution failed\n");
+      exit(1);
+    }
+  } else {
+    while (wait(&status) != pid) {
+      // wait for something
+    }
+  }
+}
+
+int main(int arg, char *argv[]) {
+  char line[1024];
+  char *args[64];
+  while (1) {
+    printf("shell> ");
+    fgets(line, 1024, stdin);
+    printf("\n");
+    printf("line: %s\n", line);
+    parse(line, args);
+    if (strcmp(args[0], "exit") == 0) {
+      exit(0);
+    }
+    execute(args);
+  }
+}
