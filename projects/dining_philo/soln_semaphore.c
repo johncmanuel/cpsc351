4d20639d255729db28988c2b2c95968244b87c93
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-11-06 13:57:02 -0800

    wip on semaphore soln

--- projects/dining_philo/soln_semaphore.c
+++ projects/dining_philo/soln_semaphore.c
@@ -1,3 +1,28 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+enum PhilosopherState { THINKING, HUNGRY, EATING };
 
-int main() { return 0; }
+const int NUM_PHILOSOPHERS = 5;
+sem_t mutex;
+sem_t s[NUM_PHILOSOPHERS];
+int state[NUM_PHILOSOPHERS];
+
+// Thread function representing the philosopher
+void run_philosopher() {}
+
+int main() {
+  pthread_t threads[NUM_PHILOSOPHERS];
+
+  // Initialize semaphores and philosopher state
+  sem_init(&mutex, 0, 1);
+  for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+    sem_init(&s[i], 0, 0);
+    state[i] = THINKING;
+  }
+
+  return 0;
+}
