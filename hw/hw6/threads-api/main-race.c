206a89a1b80b95376ea4f77a49493e4c553b329f
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-11-06 11:29:16 -0800

    setup proj, hw6

--- hw/hw6/threads-api/main-race.c
+++ hw/hw6/threads-api/main-race.c
@@ -3,16 +3,21 @@
 #include "common_threads.h"
 
 int balance = 0;
+pthread_mutex_t lock;
 
-void* worker(void* arg) {
-    balance++; // unprotected access 
-    return NULL;
+void *worker(void *arg) {
+  pthread_mutex_lock(&lock);
+  balance++; // unprotected access
+  pthread_mutex_unlock(&lock);
+  return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    pthread_t p;
-    Pthread_create(&p, NULL, worker, NULL);
-    balance++; // unprotected access
-    Pthread_join(p, NULL);
-    return 0;
+  pthread_t p;
+  Pthread_create(&p, NULL, worker, NULL);
+  pthread_mutex_lock(&lock);
+  balance++; // unprotected access
+  pthread_mutex_unlock(&lock);
+  Pthread_join(p, NULL);
+  return 0;
 }
--- hw/hw6/threads-api/main-signal.c
+++ hw/hw6/threads-api/main-signal.c
@@ -4,17 +4,17 @@
 
 int done = 0;
 
-void* worker(void* arg) {
-    printf("this should print first\n");
-    done = 1;
-    return NULL;
+void *worker(void *arg) {
+  printf("this should print first\n");
+  done = 1;
+  return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    pthread_t p;
-    Pthread_create(&p, NULL, worker, NULL);
-    while (done == 0)
-	;
-    printf("this should print last\n");
-    return 0;
+  pthread_t p;
+  Pthread_create(&p, NULL, worker, NULL);
+  while (done == 0)
+    ;
+  printf("this should print last\n");
+  return 0;
 }
