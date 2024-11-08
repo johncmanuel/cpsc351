951c076f9b43e9d76e483a6f5a9a19b30b00c0ac
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-11-07 16:38:18 -0800

    wip on semaphore soln

--- projects/dining_philo/soln_semaphore.c
+++ projects/dining_philo/soln_semaphore.c
@@ -4,16 +4,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define NUM_PHILOSOPHERS 5
+
 enum PhilosopherState { THINKING, HUNGRY, EATING };
 
-const int NUM_PHILOSOPHERS = 5;
-
-// Create a semaphore for each philosopher
+// Create a semaphore for each philosopher and a global lock
 sem_t mutex;
-sem_t s[NUM_PHILOSOPHERS];
+sem_t semaphore_philo[NUM_PHILOSOPHERS];
 
-// Track state of each philosopher
+// Track global state of each philosopher
 int state[NUM_PHILOSOPHERS];
+
+void perform_check(int i) {
+  if (state[i] == HUNGRY) {
+  }
+}
 
 // Thread function representing the philosopher
 void *run_philosopher(void *arg) {
@@ -24,27 +29,35 @@ void *run_philosopher(void *arg) {
     sleep(1);
 
     printf("Philosopher %d is hungry\n", i);
-    // Pick up fork
+    // Pick up fork when hungry
     sem_wait(&mutex);
     state[i] = HUNGRY;
+    perform_check(i);
     sem_post(&mutex);
-    sem_wait(&s[i]);
+    sem_wait(&semaphore_philo[i]);
   }
 }
 
 int main() {
   pthread_t threads[NUM_PHILOSOPHERS];
+  int philo_id[NUM_PHILOSOPHERS];
 
   // Initialize semaphores and philosopher state
   sem_init(&mutex, 0, 1);
   for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-    sem_init(&s[i], 0, 0);
+    sem_init(&semaphore_philo[i], 0, 0);
     state[i] = THINKING;
   }
 
   // Create threads
   for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-    pthread_create(&threads[i], NULL, run_philosopher, (void *)&i);
+    philo_id[i] = i;
+    pthread_create(&threads[i], NULL, run_philosopher, (void *)&philo_id[i]);
+  }
+
+  // Perform waits on threads
+  for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+    pthread_join(threads[i], NULL);
   }
 
   return 0;
