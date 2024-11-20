9f308298bef1416825124698395397ee68a8bb44
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-11-19 22:13:11 -0800

    monitor soln wip

--- projects/dining_philo/soln_monitor.cpp
+++ projects/dining_philo/soln_monitor.cpp
@@ -1,75 +1,101 @@
 #include <array>
-// #include <chrono>
+#include <chrono>
 #include <condition_variable>
 #include <iostream>
 #include <mutex>
 #include <thread>
 #include <vector>
 
+#define N 5
+#define SLEEP_TIME_MS 1000
+
 class Monitor {
 
 private:
-  static const int NUM_PHILOSOPHERS = 5;
-  enum { THINKING, HUNGRY, EATING } state[NUM_PHILOSOPHERS];
-  std::mutex mtx;
+  enum { THINKING, HUNGRY, EATING } state[N];
+  std::mutex m;
+  int print_count = 0;
 
   // represent each philosopher as a condition variable
-  std::array<std::condition_variable, NUM_PHILOSOPHERS> cv;
+  std::array<std::condition_variable, N> cv;
 
   void can_eat(int i) {
-    int left = (i + 4) % NUM_PHILOSOPHERS;
-    int right = (i + 1) % NUM_PHILOSOPHERS;
+    int left = (i + 4) % N;
+    int right = (i + 1) % N;
     if (state[left] != EATING && state[i] == HUNGRY && state[right] != EATING) {
       state[i] = EATING;
+      log_state();
       cv[i].notify_one();
     }
   }
 
+  void log_state() {
+    std::cout << "\n";
+    for (int i = 0; i < N; i++) {
+      std::cout << "P" << i << " ";
+
+      switch (state[i]) {
+      case THINKING:
+        std::cout << "THINKING   ";
+        break;
+      case HUNGRY:
+        std::cout << "HUNGRY     ";
+        break;
+      case EATING:
+        std::cout << "EATING     ";
+        break;
+      }
+    }
+
+    print_count++;
+
+    if (print_count % 5 == 0)
+      std::cout << "\n";
+
+    std::cout.flush();
+  }
+
 public:
   Monitor() {
-    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+    for (int i = 0; i < N; i++) {
       state[i] = THINKING;
     }
+    log_state();
   }
+
   void get(int i) {
-    std::unique_lock<std::mutex> lock(mtx);
+    std::unique_lock<std::mutex> lock(m);
     state[i] = HUNGRY;
+
+    log_state();
+
     can_eat(i);
+
     if (state[i] != EATING) {
       cv[i].wait(lock);
     }
   }
-  void put(int i) {
-    std::unique_lock<std::mutex> lock(mtx);
 
-    int left = (i + 4) % NUM_PHILOSOPHERS;
-    int right = (i + 1) % NUM_PHILOSOPHERS;
+  void put(int i) {
+    std::unique_lock<std::mutex> lock(m);
+
+    int left = (i + 4) % N;
+    int right = (i + 1) % N;
 
     state[i] = THINKING;
+
+    log_state();
+
     can_eat(left);
     can_eat(right);
   }
 
-  // NOTE: using std::cout or other iostream functions can lead to segementation
-  // faults since they are shared resources
   void run(int i) {
-    for (int k = 0; k < 10; k++) {
-      // print state of each philosopher
-      // for (int j = 0; j < NUM_PHILOSOPHERS; j++) {
-      //   printf("\t");
-      // }
-      printf("test");
-      // std::cout << "Thread id=" << std::this_thread::get_id() << std::endl;
-      // std::cout << "P#" << i << " " << state[i] << std::endl;
-      // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-      // Get chopsticks
-      // get(i);
-      //
-      // // Eat with chopsticks
-      // // ...
-      //
-      // put(i);
+    while (true) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME_MS));
+      get(i);
+      std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME_MS));
+      put(i);
     }
   }
 };
@@ -78,7 +104,7 @@ int main() {
   Monitor m;
   std::vector<std::thread> philosophers;
 
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < N; i++) {
     philosophers.push_back(std::thread(&Monitor::run, &m, i));
   }
 
